dc229c6768a2c36788a907dfcc07acc9dc4667c3
daseinkapital <drewbaer@vt.edu>
2017-03-05 19:13:07 -0500

    Removed old files

--- Bowling Score/run_tests.R
+++
@@ -1,3 +0,0 @@
-library(testthat)
-source("C:/Users/Andrew/Documents/Junior Year/Excella/Excella-Katas/Bowling Score/BowlingScorer.R")
-test_file("C:/Users/Andrew/Documents/Junior Year/Excella/Excella-Katas/Bowling Score/test_BowlingScorer.R")
